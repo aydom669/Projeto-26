5e989137b51cf4594828842a3315c6d365d87e3f
AydomSantos <81184313+aydom669@users.noreply.github.com>
2022-04-13 16:42:22 -0300

    Manutenção e modificaçoes

--- Script/main.js
+++ Script/main.js
@@ -1,15 +1,5 @@
 
-// Regras Do site
 
-function addText(){
-  let objC = document.querySelector('.Regra__text').style.display;
-  if(objC == "none"){
-    document.querySelector('.Regra__text').style.display = 'block';
-  }else{
-    document.querySelector('.Regra__text').style.display = 'none';
-  }
-
-}
 
 // menu moba
 
